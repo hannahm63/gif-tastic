f900b93e638faeffc1ff03bc6c80630495ce6254
hannahm63 <52358146+hannahm63@users.noreply.github.com>
2019-09-26 23:49:57 -0700

    stabbed out html, basic bootstrap grid layout, and js steps and added reset.css file

---
+++ assets/javascript/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+
+    // Topics Array variable
+    let topics = [];
+
+    // for loop through topics array
+
+    // function to display gifs
+         // AJAX variable - apiKey, queryURL, 
+         let apiKey = "7NE8PrWddIIhdz9iyhXVesPuVOBrYsNy";
+         let queryURL;
+
+          // ajax call to giphy api
+              // response
+                  // prepend gifs and rating to gif display area
+              // error
+
+
+    // function on topic button click
+        // run gif display function based on button value
+
+    // function on click
+        // append user input to topics array thereby adding new button
+
+
+});
